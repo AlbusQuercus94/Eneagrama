ce32386838a648fa2b710e63e376bbfc3c3d3522
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-08 11:52:39 -0300

    Update main.js
    Atualizei usando módulos de js para importar dos dados do arquivo dados js.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+import {dados} from "./dados.js"
 
-const data = 
-    $.getJSON("./dados.json", function (dados){
-        perfeicao = dados.perfeicao
-        return perfeicao
-    })
+const {perfeicao, pretatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados
 
-console.log(data)
+for(let x in desempate.cartoes){
+    if(desempate.cartoes[x].empate1===3 && desempate.cartoes[x].empate2===6){
+        console.log(desempate.cartoes[x].pergunta)
+        console.log(desempate.cartoes[x].opcao1)
+        console.log(desempate.cartoes[x].opcao2)
+    }
+}
