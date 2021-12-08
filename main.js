52e4a7233f44c659856e54de681c0a9befd0f733
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-08 11:53:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import {dados} from "./dados.js"
+import {dados} from "./dados.js" //Importando os dados
 
-const {perfeicao, pretatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados
+const {perfeicao, pretatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados // Criando constantes com cada tipo de carta eneatipo e desempate.
 
 for(let x in desempate.cartoes){
     if(desempate.cartoes[x].empate1===3 && desempate.cartoes[x].empate2===6){
