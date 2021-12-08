d30b1aa4e79a341a8b4bc04f86eaad36cb1ec34f
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-08 20:22:25 -0300

    Update Frontend
    Atualização da estética da página.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import {dados} from "./dados.js" //Importando os dados
 
-const {perfeicao, pretatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados // Criando constantes com cada tipo de carta eneatipo e desempate.
+const eneatipo = Object.keys(dados) //Para poder alterar as cores do css.
+
+const {perfeicao, prestatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados // Criando constantes com cada tipo de carta eneatipo e desempate.
 
 for(let x in desempate.cartoes){
     if(desempate.cartoes[x].empate1===3 && desempate.cartoes[x].empate2===6){
@@ -9,3 +11,11 @@ for(let x in desempate.cartoes){
         console.log(desempate.cartoes[x].opcao2)
     }
 }
+
+
+
+var content = document.querySelector('#content')
+
+content.innerHTML = `<p>${perfeicao.cartoes[2].afirmacao}</p>`
+content.style.color = `var(--${eneatipo[8]}-txt-color)`
+document.querySelector('.card').style.backgroundColor = `var(--${eneatipo[8]}-bg-color)`
