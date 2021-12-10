c2a894044315bb035bf3e006c8566447da42be91
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-09 22:57:16 -0300

    Creating DOM Function

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const eneatipo = Object.keys(dados) //Para poder alterar as cores do css.
 
 const {perfeicao, prestatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados // Criando constantes com cada tipo de carta eneatipo e desempate.
 
+const content = document.querySelector('#content')
+const main = document.querySelector('#main')
+
 for(let x in desempate.cartoes){
     if(desempate.cartoes[x].empate1===3 && desempate.cartoes[x].empate2===6){
         console.log(desempate.cartoes[x].pergunta)
@@ -13,9 +16,23 @@ for(let x in desempate.cartoes){
 }
 
 
+var btn = document.querySelector('#iniciar')
 
-var content = document.querySelector('#content')
+function iniciar(){
+    const card = document.createElement('div');
+    card.className="card";
+    const cardText = document.createTextNode('Texto')
+    card.appendChild(cardText)
+    console.log(card);
+    main.appendChild(card);
+    main.removeChild(btn)
 
-content.innerHTML = `<p>${perfeicao.cartoes[2].afirmacao}</p>`
-content.style.color = `var(--${eneatipo[8]}-txt-color)`
-document.querySelector('.card').style.backgroundColor = `var(--${eneatipo[8]}-bg-color)`
+}
+
+
+
+btn.addEventListener('click', iniciar)
+
+// content.innerHTML = `<p>${perfeicao.cartoes[2].afirmacao}</p>`
+// content.style.color = `var(--${eneatipo[8]}-txt-color)`
+// document.querySelector('.card').style.backgroundColor = `var(--${eneatipo[8]}-bg-color)`
