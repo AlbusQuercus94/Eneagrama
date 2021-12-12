12509bbc8165de8ed9a145e93b1589f19d48ff31
Pedro Albuquerque <65203143+AlbusQuercus94@users.noreply.github.com>
2021-12-12 14:02:06 -0300

    Passar das cartas e tipo
    Adição para passar as cartas e contabilizar os concordos e os discordos.

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 import {dados} from "./dados.js" //Importando os dados
 
-const eneatipo = Object.keys(dados) //Para poder alterar as cores do css.
+const novetipo = Object.keys(dados) //Para poder alterar as cores do css.
 
 const {perfeicao, prestatividade, performance, profundidade, privacidade, planejamento, prazer, poder, paz, desempate} = dados // Criando constantes com cada tipo de carta eneatipo e desempate.
 
+//DOM
 const content = document.querySelector('#content')
 const main = document.querySelector('#main')
 const card = document.querySelector('#card')
+const comecar = document.querySelector('#iniciar')
+const btnAgree = document.querySelector('#agree')
+const btnDisagree = document.querySelector('#disagree')
+
+var teste = dados[novetipo[0]]
+var carta = 0
+var tipo = 0
+
 
 for(let x in desempate.cartoes){
     if(desempate.cartoes[x].empate1===3 && desempate.cartoes[x].empate2===6){
@@ -16,18 +25,55 @@ for(let x in desempate.cartoes){
     }
 }
 
+for (let x in dados){
+    if(dados[x] == 'desempate'){
+        continue
+    }else{
+        dados[x].agree = 0;
+        dados[x].disagree = 0;
+    }
+}
 
-var comecar = document.querySelector('#iniciar')
 
 function iniciar(){
     comecar.style.visibility = 'hidden'
     card.style.visibility = 'visible'
+    
+    content.innerHTML = `<p>${teste.cartoes[carta].afirmacao}</p>`
+    content.style.color = `var(--${novetipo[tipo]}-txt-color)`
+    document.querySelector('.card').style.backgroundColor = `var(--${novetipo[tipo]}-bg-color)`
 }
 
 
+btnAgree.addEventListener('click', () => {
+    if(carta>7){
+        carta = 0
+        tipo++
+    }else{
+        carta++
+    }
+    var teste = dados[novetipo[tipo]]
+    perfeicao.agree+=1;
+    console.log(teste, carta)
+    content.innerHTML = `<p>${teste.cartoes[carta].afirmacao}</p>`
+    content.style.color = `var(--${novetipo[tipo]}-txt-color)`
+    document.querySelector('.card').style.backgroundColor = `var(--${novetipo[tipo]}-bg-color)`
+})
+
+btnDisagree.addEventListener('click', () => {
+    if(carta>7){
+        carta = 0
+        tipo++
+    }else{
+        carta++
+    }
+    var teste = dados[novetipo[tipo]]
+    teste.disagree+=1;
+    console.log(teste, carta)
+    content.innerHTML = `<p>${teste.cartoes[carta].afirmacao}</p>`
+    content.style.color = `var(--${novetipo[tipo]}-txt-color)`
+    document.querySelector('.card').style.backgroundColor = `var(--${novetipo[tipo]}-bg-color)`
+})
+
 
 comecar.addEventListener('click', iniciar)
-
-content.innerHTML = `<p>${performance.cartoes[3].afirmacao}</p>`
-content.style.color = `var(--${eneatipo[8]}-txt-color)`
-document.querySelector('.card').style.backgroundColor = `var(--${eneatipo[8]}-bg-color)`
